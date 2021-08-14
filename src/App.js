ce03c7eb81facb6588343feb7330c6fca552bb1a
VengerAlex <alexvenger90@gmail.com>
2021-08-14 12:36:29 +1200

    fetch data from db

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import {useEffect, useState} from "react";
+
 const App = (props) => {
+    const APP_ID = '50c847f9';
+    const APP_KEY = '745c65a2aa67a5245b282c4d893b7772';
 
-  return (
-      <h1>hello</h1>
+    const [recipe, setRecipes] = useState([]);
 
+    // useEffect(async () => {
+    //     getRecipe()
+    // }, [])
+
+    const getRecipe = async () => {
+        const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+        const data = await response.json();
+
+        setRecipes(data.hits);
+    }
+
+
+
+    return (
+      <div className='App'>
+          <form className='search-form'>
+              <input
+                  className='search-bar'
+                  type="text"/>
+              <button  className='search-button' type='submit'>
+                  SUMBIT
+              </button>
+          </form>
+      </div>
   )
 }
 
